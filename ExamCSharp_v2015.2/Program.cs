c497e26c4894f0b36e9f6788cf70b3b734013efb
Tomasz Mikuczewski <miktomi15@gmail.com>
2017-01-15 00:05:43 +0100

    Zadanie 4 (Resolved #4)
    Dodano:
    - rozwiązanie zadania 4. z ED 2015.2

--- ExamCSharp_v2015.2/Program.cs
+++ ExamCSharp_v2015.2/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ExamCSharp_v2015._2
 {
@@ -10,6 +6,25 @@ namespace ExamCSharp_v2015._2
 	{
 		static void Main(string[] args)
 		{
+			// zad04
+			FunkcjaKwadratowa
+				fk1 = new FunkcjaKwadratowa(2, -3, 1),
+				fk2 = new FunkcjaKwadratowa(2, 3, 1),
+				fk3 = new FunkcjaKwadratowa(2, -3, -1),
+				fk4 = new FunkcjaKwadratowa(2, 0, 1),
+				fk5 = new FunkcjaKwadratowa(2, -3, 0),
+				fk6 = new FunkcjaKwadratowa(-2, -3, -1);
+			Console.WriteLine($"1. {fk1}");
+			Console.WriteLine($"2. {fk2}");
+			Console.WriteLine($"3. {fk3}");
+			Console.WriteLine($"4. {fk4}");
+			Console.WriteLine($"5. {fk5}");
+			Console.WriteLine($"6. {fk6}");
+
+			double[,] pierwiastki = fk1.Pierwiastki();
+			Console.WriteLine($"Ad1. x1: {pierwiastki[0, 0]}, x2: {pierwiastki[1, 0]}");
+			// /zad04
+			Console.WriteLine();
 		}
 	}
 }
--- ExamCSharp_v2015.2/Zad04.cs
+++ ExamCSharp_v2015.2/Zad04.cs
@@ -1,12 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using System.Numerics;
 
 namespace ExamCSharp_v2015._2
 {
-	class Zad04
+	public class FunkcjaKwadratowa
 	{
+		public FunkcjaKwadratowa(double a, double b, double c)
+		{
+			if (a == 0D)
+			{
+				throw new ArgumentException("Wartość parametru musi być różna od zera!", nameof(this.A));
+			}
+			this.A = a;
+			this.B = b;
+			this.C = c;
+		}
+
+		public double A { get; set; }
+		public double B { get; set; }
+		public double C { get; set; }
+
+		public double Delta => (Math.Pow(this.B, 2) - (4 * this.A * this.C));
+
+		public override string ToString()
+		{
+			string
+				a = $"{this.A}x^2",
+				b = (this.B != 0D) ? (((this.B >= 0D) ? " + " : " - ") + Math.Abs(this.B) + "x") : string.Empty,
+				c = (this.C != 0D) ? (((this.C >= 0D) ? " + " : " - ") + Math.Abs(this.C)) : string.Empty;
+
+			return $"{a}{b}{c}";
+		}
+
+		public double[,] Pierwiastki()
+		{
+			double delta = this.Delta;
+			double aa = (2 * this.A);
+			if (delta >= 0D)
+			{
+				double pierwiastekDelta = Math.Sqrt(delta);
+				return new double[,]
+				{
+					{ ((-this.B - pierwiastekDelta) / aa), 0 },
+					{ ((-this.B + pierwiastekDelta) / aa), 0 }
+				};
+			}
+			else
+			{
+				//TM: Treść zadania wymusza typ zwracany double[,] co uniemożliwia obliczenie pierwiastków w dziedzinie zespolonej.
+				//double pierwiastekDelta = Math.Sqrt(Math.Abs(delta));
+				//Complex
+				//	pierwiastek1 = new Complex(0, pierwiastekDelta),
+				//	pierwiastek2 = new Complex(0, -pierwiastekDelta);
+				//
+				//return new Complex[,]
+				//{
+				//	{ ((-this.B + pierwiastek1) / aa), 0 },
+				//	{ ((-this.B + pierwiastek2) / aa), 0 }
+				//};
+
+				throw new NotSupportedException();
+			}
+		}
 	}
 }
