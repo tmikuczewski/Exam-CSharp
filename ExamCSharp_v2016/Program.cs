2a8bf93d8c2eb795f455fced455e5247d2e0b642
Tomasz Mikuczewski <miktomi15@gmail.com>
2017-01-15 13:26:06 +0100

    Egzamin 2016 (Issue #7)
    Dodano:
    - strukturę projektu rozwiązującego ED 2016

---
+++ ExamCSharp_v2016/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamCSharp_v2016
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
---
+++ ExamCSharp_v2016/Zad10.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamCSharp_v2016
+{
+	class Zad10
+	{
+	}
+}
