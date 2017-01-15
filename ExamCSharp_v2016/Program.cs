a43cd7bc75c71aa7071115bf3b4192105ce94f6a
Tomasz Mikuczewski <miktomi15@gmail.com>
2017-01-15 14:12:23 +0100

    Zadanie 8 (Resolved #8)
    Dodano:
    - rozwiązanie zadania 8. z ED 2016

--- ExamCSharp_v2016/Program.cs
+++ ExamCSharp_v2016/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ExamCSharp_v2016
 {
@@ -10,6 +6,24 @@ namespace ExamCSharp_v2016
 	{
 		static void Main(string[] args)
 		{
+			// zad08
+			Wektor w1 = new Wektor(5);
+			Wektor w2 = new Wektor(5, 2);
+			Wektor w3 = new Wektor(1, 2, 3, 4, 5);
+			Wektor w4 = new Wektor(5, 4, 3, 2, 1);
+
+			double iloczynSkalarny = Wektor.IloczynSkalarny(w3, w4);
+			Wektor w = w3 + w4;
+
+			Console.WriteLine(w1);
+			Console.WriteLine(w2);
+			Console.WriteLine(w3);
+			Console.WriteLine(w4);
+			Console.WriteLine(w3.Dim.ToString());
+			Console.WriteLine(w4[2].ToString());
+			Console.WriteLine(iloczynSkalarny.ToString());
+			Console.WriteLine(w);
+			// /zad08
 		}
 	}
 }
--- ExamCSharp_v2016/Zad08.cs
+++ ExamCSharp_v2016/Zad08.cs
@@ -1,12 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ExamCSharp_v2016
 {
-	class Zad08
+	public sealed class Wektor : List<double>
 	{
+		public Wektor(int n)
+			: this(n, 1D)
+		{
+		}
+		public Wektor(int n, double a)
+		{
+			for (int i = 0; i < n; i++)
+			{
+				this.Add(a);
+			}
+		}
+		public Wektor(params double[] lista)
+		{
+			if (lista.Length > int.MaxValue)
+			{
+				throw new ArgumentOutOfRangeException(nameof(lista));
+			}
+			this.AddRange(lista);
+		}
+
+		public int Dim => this.Count;
+
+		public override string ToString()
+		{
+			return (this.Dim > 0) ? $"{{{string.Join(",", this)}}}" : "pusty";
+		}
+
+		public static double IloczynSkalarny(Wektor lewy, Wektor prawy)
+		{
+			if (lewy.Dim != prawy.Dim)
+			{
+				throw new ArgumentException("Wymiary wektorów nie zgadzają się!");
+			}
+
+			double wynik = 0;
+			for (int i = 0; i < lewy.Dim; i++)
+			{
+				wynik += (lewy[i] * prawy[i]);
+			}
+			return wynik;
+		}
+
+		public static Wektor operator +(Wektor lewy, Wektor prawy)
+		{
+			if (lewy.Dim != prawy.Dim)
+			{
+				throw new ArgumentException("Wymiary wektorów nie zgadzają się!");
+			}
+
+			Wektor wynik = new Wektor(lewy.Dim);
+			for (int i = 0; i < lewy.Dim; i++)
+			{
+				wynik[i] = (lewy[i] + prawy[i]);
+			}
+			return wynik;
+		}
 	}
 }
